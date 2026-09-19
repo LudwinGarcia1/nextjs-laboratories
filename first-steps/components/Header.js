36bdca01ebaa88c93e41237c0409cac78320fc47
Ludwin García Gaytán <ludwin.garcia@grupoaov.com>
2026-09-18 20:38:43 -0600

    feat: add about and blog routes with shared Header component

---
+++ first-steps/components/Header.js
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <header>
+            <a href="/">UTVT</a>
+            <nav>
+                <a href="/about">Acerca de</a>
+                <a href="/blog">Blog</a>
+            </nav>
+        </header>
+    );
+}
